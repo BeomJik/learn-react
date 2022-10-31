b00607c867d92f350d08324f184be117bf1f41c4
BeomGeun <beomjikdp@gmail.com>
2022-10-31 18:09:07 +0900

    list

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,21 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 
-function List(){
+const User = ({userData}) => {
+    return (
+        <tr>
+            <td>{userData.name}</td>
+            <td>{userData.email}</td>
+        </tr>
+    )
+}
 
-    const userList = () =>{
-        const users = [
-            {email : 'gamst@gmail.com',name : '김인직'},
-            {email : 'kim@gmail.com',name : '김병지'},
-            {email : 'lee@gmail.com',name : '이삼이'},
-            {email : 'park@gmail.com',name : '창근이'}
-        ];
-    }
+const UserList = () =>{
+
+    const users = [
+        {email : 'gamst@gmail.com', name : '김인직'},
+        {email : 'kim@gmail.com', name : '김병지'},
+        {email : 'lee@gmail.com', name : '이삼이'},
+        {email : 'park@gmail.com', name : '창근이'}
+    ];
 
     return(
-        <div>
-
-        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>이름</th>
+                    <th>이메일</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map( user => <User userData={user} /> )}
+            </tbody>
+        </table>
     );
 }
 
-export default List;
+export default UserList;
